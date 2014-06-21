4a9874b504c806ef13413470ee155b1d8c6ab821
Ari Malinen <ari.malinen@gmail.com>
2014-06-21 21:38:03 +0300

    functions

--- nbwmon.c
+++ nbwmon.c
@@ -12,23 +12,29 @@ int delay = 2;
 int ident = LEN / 4;
 
 struct data {
+	// KB/s
 	float rxs;
 	float txs;
+	// peak KB/s
 	float rxmax;
 	float txmax;
 	float max;
+	// total traffic
 	int rx;
 	int tx;
 	int rx2;
 	int tx2;
+	// packets
 	int rxp;
 	int txp;
+	// errors
 	int rxe;
 	int txe;
-	float rxgraphs[80];
-	float txgraphs[80];
-	bool rxgraph[10][80];
-	bool txgraph[10][80];
+	// bandwidth graph
+	float rxgraphs[LEN];
+	float txgraphs[LEN];
+	bool rxgraph[10][LEN];
+	bool txgraph[10][LEN];
 };
 
 int arg(int argc, char *argv[]) {
@@ -38,12 +44,14 @@ int arg(int argc, char *argv[]) {
 		if (!strcmp("-d", argv[i])) {
 			if (argv[i+1] == NULL || argv[i+1][0] == '-') {
 				fprintf(stderr, "error: -d needs parameter\n");
+				endwin();
 				exit(1);
 			}
 			delay = strtol(argv[++i], NULL, 0);
 		} else if (!strcmp("-i", argv[i])) {
 			if (argv[i+1] == NULL || argv[i+1][0] == '-') {
 				fprintf(stderr, "error: -i needs parameter\n");
+				endwin();
 				exit(1);
 			}
 			strncpy(iface, argv[++i], LEN);
@@ -52,6 +60,7 @@ int arg(int argc, char *argv[]) {
 			fprintf(stderr, "-h         help\n");
 			fprintf(stderr, "-d <delay> delay\n");
 			fprintf(stderr, "-i <iface> interface\n");
+			endwin();
 			exit(1);
 		}
 	}
@@ -75,11 +84,145 @@ long fgetint(char *file) {
 	return strtol(line, NULL, 0);
 }
 
-int main(int argc, char *argv[]) {
-	struct data d = {0};
+void printstats(struct data d) {
+	int x, y;
+	char str[LEN+1];
+
+	clear();
+
+	sprintf(str, "interface: %s delay: %d sec\n\n", iface, delay);
+	addstr(str);
+	sprintf(str, "%*s %*s %s\n", ident+3, "RX", ident-3, "", "TX");
+	addstr(str);
+
+	sprintf(str, "%-*s %-*.1f %-*.1f KB/s\n", ident, "speed", ident, d.rxs, ident, d.txs);
+	addstr(str);
+	sprintf(str, "%-*s %-*.1f %-*.1f KB/s\n", ident, "peak", ident, d.rxmax, ident, d.txmax);
+	addstr(str);
+	sprintf(str, "%-*s %-*.1f %-*.1f MB\n", ident, "total", ident,
+			(float) d.rx2 / 1024000, ident, (float) d.tx2 / 1024000);
+	addstr(str);
+	sprintf(str, "%-*s %-*d %-*d total\n", ident, "packets", ident, d.rxp, ident, d.txp);
+	addstr(str);
+	sprintf(str, "%-*s %-*d %-*d total\n\n", ident, "errors", ident, d.rxe, ident, d.txe);
+	addstr(str);
+
+	for (x = 9; x >= 0; x--) {
+		for (y = 0; y < LEN; y++) {
+			if (d.rxgraph[x][y]) {
+				addch('#');
+			} else {
+				addch(' ');
+			}
+		}
+		addch('\n');
+	}
+
+	addch('\n');
+
+	for (x = 0; x < 10; x++) {
+		for (y = 0; y < LEN; y++) {
+			if (d.txgraph[x][y]) {
+				addch('#');
+			} else {
+				addch(' ');
+			}
+		}
+		addch('\n');
+	}
+
+	refresh();
+}
+
+struct data getdata(struct data d) {
+	char str[LEN+1];
+
+	sprintf(str, "/sys/class/net/%s/statistics/rx_packets", iface);
+	d.rxp = fgetint(str);
+	sprintf(str, "/sys/class/net/%s/statistics/tx_packets", iface);
+	d.txp = fgetint(str);
+
+	sprintf(str, "/sys/class/net/%s/statistics/rx_errors", iface);
+	d.rxe = fgetint(str);
+	sprintf(str, "/sys/class/net/%s/statistics/tx_errors", iface);
+	d.txe = fgetint(str);
+
+	sprintf(str, "/sys/class/net/%s/statistics/rx_bytes", iface);
+	d.rx = fgetint(str);
+	sprintf(str, "/sys/class/net/%s/statistics/tx_bytes", iface);
+	d.tx = fgetint(str);
+
+	sleep(delay);
+
+	sprintf(str, "/sys/class/net/%s/statistics/rx_bytes", iface);
+	d.rx2 = fgetint(str);
+	sprintf(str, "/sys/class/net/%s/statistics/tx_bytes", iface);
+	d.tx2 = fgetint(str);
+
+	d.rxs = (float) (d.rx2 - d.rx) / 1024 / delay;
+	d.txs = (float) (d.tx2 - d.tx) / 1024 / delay;
+
+	d.max = d.rxmax = (d.rxs > d.max ? d.rxs : d.max);
+	d.max = d.txmax = (d.txs > d.max ? d.txs : d.max);
+
+	return d;
+}
+
+struct data updategraph(struct data d) {
 	int x, y;
 	float i, j;
-	char str[LEN+1];
+
+	// scale graph
+	if (d.max == d.rxs || d.max == d.txs) {
+		for (x = 0; x < 10; x++) {
+			for (y = 0; y < LEN; y++) {
+				i = (float) d.rxgraphs[y] / d.max * 10;
+				j = (float) d.txgraphs[y] / d.max * 10;
+				if (i > x)
+					d.rxgraph[x][y] = true;
+				else
+					d.rxgraph[x][y] = false;
+				if (j > x)
+					d.txgraph[x][y] = true;
+				else
+					d.txgraph[x][y] = false;
+			}
+		}
+	}
+
+	// move graph
+	for (x = 0; x < 10; x++) {
+		for (y = 0; y < LEN-1; y++) {
+			d.rxgraph[x][y] = d.rxgraph[x][y+1];
+			d.txgraph[x][y] = d.txgraph[x][y+1];
+		}
+	}
+	for (y = 0; y < LEN-1; y++) {
+		d.rxgraphs[y] = d.rxgraphs[y+1];
+		d.txgraphs[y] = d.txgraphs[y+1];
+	}
+
+	// create new graph line
+	i = (float) d.rxs / d.max * 10;
+	j = (float) d.txs / d.max * 10;
+	for (x = 0; x < 10; x++) {
+		if (i > x)
+			d.rxgraph[x][LEN-1] = true;
+		else
+			d.rxgraph[x][LEN-1] = false;
+		if (j > x)
+			d.txgraph[x][LEN-1] = true;
+		else
+			d.txgraph[x][LEN-1] = false;
+	}
+	d.rxgraphs[LEN-1] = d.rxs;
+	d.txgraphs[LEN-1] = d.txs;
+
+	return d;
+}
+
+int main(int argc, char *argv[]) {
+	struct data d = {.rxs = 0, .txs = 0, .rxmax = 0, .txmax = 0, .max = 0};
 
 	arg(argc, argv);
 
@@ -88,126 +231,11 @@ int main(int argc, char *argv[]) {
 	curs_set(0);
 
 	while (1) {
-		// print stats
-		clear();
+		printstats(d);
 
-		sprintf(str, "interface: %s delay: %d sec\n\n", iface, delay);
-		addstr(str);
-		sprintf(str, "%*s %*s %s\n", ident+3, "RX", ident-3, "", "TX");
-		addstr(str);
+		d = getdata(d);
 
-		sprintf(str, "%-*s %-*.1f %-*.1f KB/s\n", ident, "speed", ident, d.rxs, ident, d.txs);
-		addstr(str);
-		sprintf(str, "%-*s %-*.1f %-*.1f KB/s\n", ident, "peak", ident, d.rxmax, ident, d.txmax);
-		addstr(str);
-		sprintf(str, "%-*s %-*.1f %-*.1f MB\n", ident, "total", ident,
-				(float) d.rx2 / 1024000, ident, (float) d.tx2 / 1024000);
-		addstr(str);
-		sprintf(str, "%-*s %-*d %-*d total\n", ident, "packets", ident, d.rxp, ident, d.txp);
-		addstr(str);
-		sprintf(str, "%-*s %-*d %-*d total\n\n", ident, "errors", ident, d.rxe, ident, d.txe);
-		addstr(str);
-
-		for (x = 9; x >= 0; x--) {
-			for (y = 0; y < LEN; y++) {
-				if (d.rxgraph[x][y]) {
-					addch('#');
-				} else {
-					addch(' ');
-				}
-			}
-			addch('\n');
-		}
-
-		addch('\n');
-
-		for (x = 0; x < 10; x++) {
-			for (y = 0; y < LEN; y++) {
-				if (d.txgraph[x][y]) {
-					addch('#');
-				} else {
-					addch(' ');
-				}
-			}
-			addch('\n');
-		}
-
-		refresh();
-
-		// get data
-		sprintf(str, "/sys/class/net/%s/statistics/rx_packets", iface);
-		d.rxp = fgetint(str);
-		sprintf(str, "/sys/class/net/%s/statistics/tx_packets", iface);
-		d.txp = fgetint(str);
-
-		sprintf(str, "/sys/class/net/%s/statistics/rx_errors", iface);
-		d.rxe = fgetint(str);
-		sprintf(str, "/sys/class/net/%s/statistics/tx_errors", iface);
-		d.txe = fgetint(str);
-
-		sprintf(str, "/sys/class/net/%s/statistics/rx_bytes", iface);
-		d.rx = fgetint(str);
-		sprintf(str, "/sys/class/net/%s/statistics/tx_bytes", iface);
-		d.tx = fgetint(str);
-
-		sleep(delay);
-
-		sprintf(str, "/sys/class/net/%s/statistics/rx_bytes", iface);
-		d.rx2 = fgetint(str);
-		sprintf(str, "/sys/class/net/%s/statistics/tx_bytes", iface);
-		d.tx2 = fgetint(str);
-
-		d.rxs = (float) (d.rx2 - d.rx) / 1024 / delay;
-		d.txs = (float) (d.tx2 - d.tx) / 1024 / delay;
-
-		d.max = d.rxmax = (d.rxs > d.max ? d.rxs : d.max);
-		d.max = d.txmax = (d.txs > d.max ? d.txs : d.max);
-
-		// scale graph
-		if (d.max == d.rxs || d.max == d.txs) {
-			for (x = 0; x < 10; x++) {
-				for (y = 0; y < LEN; y++) {
-					i = (float) d.rxgraphs[y] / d.max * 10;
-					j = (float) d.txgraphs[y] / d.max * 10;
-					if (i > x)
-						d.rxgraph[x][y] = true;
-					else
-						d.rxgraph[x][y] = false;
-					if (j > x)
-						d.txgraph[x][y] = true;
-					else
-						d.txgraph[x][y] = false;
-				}
-			}
-		}
-
-		// move graph
-		for (x = 0; x < 10; x++) {
-			for (y = 0; y < LEN-1; y++) {
-				d.rxgraph[x][y] = d.rxgraph[x][y+1];
-				d.txgraph[x][y] = d.txgraph[x][y+1];
-			}
-		}
-		for (y = 0; y < LEN-1; y++) {
-			d.rxgraphs[y] = d.rxgraphs[y+1];
-			d.txgraphs[y] = d.txgraphs[y+1];
-		}
-
-		// create new graph line
-		i = (float) d.rxs / d.max * 10;
-		j = (float) d.txs / d.max * 10;
-		for (x = 0; x < 10; x++) {
-			if (i > x)
-				d.rxgraph[x][LEN-1] = true;
-			else
-				d.rxgraph[x][LEN-1] = false;
-			if (j > x)
-				d.txgraph[x][LEN-1] = true;
-			else
-				d.txgraph[x][LEN-1] = false;
-		}
-		d.rxgraphs[LEN-1] = d.rxs;
-		d.txgraphs[LEN-1] = d.txs;
+		d = updategraph(d);
 	}
 
 	endwin();
